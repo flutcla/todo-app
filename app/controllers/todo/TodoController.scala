ebbf5c1fe0a2085762254782b76b5386a75bcfab
flutcla <flutcla@gmail.com>
2023-07-04 10:49:45 +0900

    get を使用しないように修正

--- app/controllers/todo/TodoController.scala
+++ app/controllers/todo/TodoController.scala
@@ -7,11 +7,12 @@ import play.api.mvc.Request
 import play.api.mvc.AnyContent
 import java.time.LocalDateTime
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration.Duration
 
 import model.ViewValueTodo
 import lib.model.Todo
+import lib.model.Category
 import lib.persistence.default
 
 @Singleton
@@ -27,21 +28,20 @@ class TodoController @Inject()(val controllerComponents: ControllerComponents) e
     Await.ready(todoFuture, Duration.Inf)
     Await.ready(categoryFuture, Duration.Inf)
     for {
-      results <- todoFuture
+      todos <- todoFuture
       categories <- categoryFuture
-    } yield (
-      Ok(views.html.todo.list(
-          vv,
-          results.map(res =>
-          (
-            res,
-            categories
-              .filter(_.id == res.v.categoryId)
-              .headOption
-              .get
-          )
-        )
-      ))
-    )
+    } yield {
+      val todoCategoryOptSeq: Seq[Option[Tuple2[Todo#EmbeddedId, Category#EmbeddedId]]] = todos.map(res =>
+        categories.filter(_.id == res.v.categoryId).headOption match {
+          case Some(cat) => Some(res, cat)
+          case None      => None
+        }
+      )
+      if (todoCategoryOptSeq.contains(None)) {
+        ???  // TODO: Category が見つからなかった際の処理を考える
+      } else {
+        Ok(views.html.todo.list(vv, todoCategoryOptSeq.flatten))
+      }
+    }
   }}
 }
