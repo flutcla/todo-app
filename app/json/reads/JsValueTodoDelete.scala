546b172ad542a510b2a847c3e3b0d871a8f04928
flutcla <flutcla@gmail.com>
2023-07-21 13:02:09 +0900

    ixias.util.json.JsonEnvReads を用いるように変更

--- app/json/reads/JsValueTodoDelete.scala
+++ app/json/reads/JsValueTodoDelete.scala
@@ -4,12 +4,12 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import lib.model.Todo
 
+import ixias.util.json.JsonEnvReads
+
 case class JsValueTodoDelete(
   id: Todo.Id
 )
-object JsValueTodoDelete {
-  implicit val todoIdReads: Reads[Todo.Id] = Reads.of[Long].map(Todo.Id.apply)
-  implicit val reads: Reads[JsValueTodoDelete] = (
-    (JsPath \ "id").read[Todo.Id](todoIdReads)
-  ).map(JsValueTodoDelete.apply)
+object JsValueTodoDelete extends JsonEnvReads {
+  implicit val todoIdReads: Reads[Todo.Id] = idAsNumberReads
+  implicit val reads: Reads[JsValueTodoDelete] = Json.reads
 }
