7b146c004c3a2da8e2fd427207aaf191a221b1a3
flutcla <flutcla@gmail.com>
2023-07-27 18:15:42 +0900

    /category/update が POST を受け取るように変更

---
+++ app/json/reads/JsValueCategory.scala
@@ -0,0 +1,23 @@
+package json.reads
+
+import java.awt.Color
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import lib.model.Category
+
+import ixias.util.json.JsonEnvReads
+
+case class JsValueCategory(
+  id:    Category.Id,
+  name:  String,
+  slug:  String,
+  color: Color
+)
+object JsValueCategory extends JsonEnvReads {
+  implicit val categoryIdReads: Reads[Category.Id] = idAsNumberReads
+  implicit val colorReads: Reads[Color] = Reads.of[String].map(colorCode =>
+    new Color(Integer.parseInt(colorCode.substring(1, 7), 16), true)
+  )
+  implicit val reads: Reads[JsValueCategory] = Json.reads
+}
